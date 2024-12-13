b7fa116592f79955d9bb90db0117908bf7a9afad
xmatute- <xmatute-@student.42urduliz.com>
2024-12-13 13:10:01 +0100

    0.3.7

--- installer.sh
+++ installer.sh
@@ -8,13 +8,14 @@ script_path=$(pwd)/kirby.sh
 
 kalias="alias kirby42='/bin/bash $script_path'"
 
-echo $kalias >> ~/.$(basename "$SHELL")rc
+echo $kalias >> ~/.bashrc
+echo $kalias >> ~/.zshrc
 
 script_path=$(pwd)/realocate.sh
 
 kalias="alias kirby_realocate='/bin/bash $script_path'"
 
-echo $kalias >> ~/.$(basename "$SHELL")rc
-
+echo $kalias >> ~/.bashrc
+echo $kalias >> ~/.zshrc
 
 echo -e "\nTo use the 'kirby42' , Please restart your terminal then run 'kirby42'\033[0m"
