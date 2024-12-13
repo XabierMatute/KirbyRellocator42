af4329eefd23d3279fe4bcdb2547a49bee7a2074
xmatute- <xmatute-@student.42urduliz.com>
2024-12-13 13:03:58 +0100

    0.3.4

--- installer.sh
+++ installer.sh
@@ -6,15 +6,15 @@ chmod +x realocate.sh
 
 script_path=$(pwd)/kirby.sh
 
-alias="alias kirby42='bin/bash $script_path'"
+kalias="alias kirby42='bin/bash $script_path'"
 
-$alias >> ~/.$(basename "$SHELL")rc
+$kalias >> ~/.$(basename "$SHELL")rc
 
 script_path=$(pwd)/realocate.sh
 
-alias="alias kirby_realocate='bin/bash $script_path'"
+kalias="alias kirby_realocate='bin/bash $script_path'"
 
-$alias >> ~/.$(basename "$SHELL")rc
+$kalias >> ~/.$(basename "$SHELL")rc
 
 
 echo -e "\nTo use the 'kirby42' , Please restart your terminal then run 'kirby42'\033[0m"
