f456927cafecb5fc99b42cf104fc44056c15d3c0
xmatute- <xmatute-@student.42urduliz.com>
2024-12-13 18:48:12 +0100

    deallocate bad upload

--- deallocate.sh
+++ deallocate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-blue='\033[0;34m'
-reset='\033[0m'
-
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <source_path> <destination_path>"
@@ -12,32 +9,29 @@ fi
 SOURCE_PATH=$1
 DESTINATION_PATH=$2
 
-# Check if the source path exists
-if [ ! -e "$SOURCE_PATH" ]; then
-    echo "Error: Source path '$SOURCE_PATH' does not exist."
+# Check if the source path is a symbolic link
+if [ ! -L "$SOURCE_PATH" ]; then
+    echo "Error: Source path '$SOURCE_PATH' is not a symbolic link."
     exit 1
 fi
 
-if [ -e "$DESTINATION_PATH" ]; then
-    read -p "Destination path '$DESTINATION_PATH' already exists and directories will be merged. Do you want to proceed anyway? (y/n): " proceed
-    if [ "$proceed" != "y" ]; then
-        echo "Operation cancelled."
-        exit 1
-    fi
+# Check if the symbolic link points to the destination path
+LINK_TARGET=$(readlink "$SOURCE_PATH")
+if [ "$LINK_TARGET" != "$DESTINATION_PATH" ]; then
+    echo "Error: Source path '$SOURCE_PATH' does not point to destination path '$DESTINATION_PATH'."
+    exit 1
 fi
 
-# Move the source to the destination
-echo "Moving '$SOURCE_PATH' to '$DESTINATION_PATH'... This might take a while if the file is large."
-echo -e "${blue}..."
-rsync -a --progress "$SOURCE_PATH"/ "$DESTINATION_PATH"
-echo -e "${reset}"
-echo "Done moving '$SOURCE_PATH' to '$DESTINATION_PATH'."
-echo "Removing the original source path '$SOURCE_PATH'..."
-rm -fr "$SOURCE_PATH"
-echo "Done removing the original source path."
+# Remove the symbolic link
+echo "Removing symbolic link '$SOURCE_PATH'..."
+rm "$SOURCE_PATH"
 
-# Create a symbolic link at the original source path pointing to the new destination
-echo "Creating a symlink at '$SOURCE_PATH' pointing to '$DESTINATION_PATH'..."
-ln -s "$DESTINATION_PATH" "$SOURCE_PATH"
+# Move the content back from destination to source using rsync
+echo "Moving content from '$DESTINATION_PATH' back to '$SOURCE_PATH'..."
+rsync -a --progress "$DESTINATION_PATH/" "$SOURCE_PATH/"
 
-echo "Moved '$SOURCE_PATH' to '$DESTINATION_PATH' and created a symlink at '$SOURCE_PATH' pointing to '$DESTINATION_PATH'."
+# Remove the destination path after moving the content
+echo "Removing the destination path '$DESTINATION_PATH'..."
+rm -rf "$DESTINATION_PATH"
+
+echo "Deallocation complete. '$SOURCE_PATH' has been restored from '$DESTINATION_PATH'."
